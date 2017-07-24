de07c944b4e3f0496706ae027090d8daf1530429
xoADDYxo <addy.7kad@gmail.com>
2017-07-24 20:43:26 +0530

    Change the orientation on x axis

--- heatmap.py
+++ heatmap.py
@@ -9,9 +9,9 @@ games=games.read_csv('ign.csv')
 games.drop(['Unnamed: 0','url'],axis=1,inplace=True)
 games.drop(games.index[516],inplace=True)	
 
-games.replace(['Action','Action, Adventure','Action, RPG','Adventure','Fighting',
-                 'Platformer','Puzzle','Racing, Action','Racing','Shooter','Simulation','Strategy','1996']
-                ,['Act','Act,adv','Act,RPG','Adv','Fight','Plat','Puzz','Rac,Act','Race','Shoot','Simltn','Strat',str(96)],inplace=True)
+games.replace(['Action, Adventure','Action, RPG','Adventure','Fighting',
+                 'Platformer','Racing, Action','Racing','Shooter','Simulation']
+                ,['Act,adv','Act,RPG','Adv','Fight','Plat','Rac,Act','Race','Shoot','Simltn'],inplace=True)
 
 max_genres=games.groupby('genre')['genre'].count()
 max_genres=max_genres[max_genres.values>200]
@@ -21,5 +21,6 @@ abc=mean_games.groupby(['release_year','genre'])['score'].mean().reset_index()
 abc=abc.pivot('release_year','genre','score')
 sns.heatmap(abc,annot=True,cmap='RdYlGn',linewidths=0.4,yticklabels='auto')
 pol.yticks(rotation='horizontal')
+pol.xticks(rotation='vertical')
 pol.title('Average Score By Genres')
 pol.show()
